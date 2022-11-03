3d595341d46f675828a138e5264bc12abfe57f50
22PHOP2D-Duurenbayar <duurenbayarOwO@Nest-101s-iMac.local>
2022-11-03 19:36:20 +0800

    some shage

--- src/Components/Review2.js
+++ src/Components/Review2.js
@@ -4,7 +4,14 @@ export const Review2 = (props) => {
     return <><div className="Container">
         <header>
             <div className="Logo">
-                
+
+            </div>
+            <div className="contain">
+                <h2 className="head">Products</h2>
+                <h2 className="head">Services</h2>
+                <h2 className="head">Contact</h2>
+                <h2 className="head">Log in</h2>
+                <h2 className="headSpecial">Get Access</h2>
             </div>
         </header>
     </div>
